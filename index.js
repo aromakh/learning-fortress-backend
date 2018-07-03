cb1bb2a500fed07ec41f65c9981fadba4c07aa50
RealFighter64 <realfighter64@gmail.com>
2018-07-03 13:37:45 +0100

    Add brick GET endpoint.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var restify = require('restify');
+const restify = require('restify');
+const admin = require('firebase-admin');
+
+const servicePath = require('./key/learning-fortress-firebase-adminsdk-1qpqj-4f47ca143b.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(servicePath),
+    databaseURL: 'https://learning-fortress.firebaseio.com'
+});
+var db = admin.firestore();
 
 var server = restify.createServer({
     name: 'learning-fortress-backend',
@@ -13,6 +22,20 @@ server.get('/hello', function(req, res, next) {
     res.send({ message: 'Hello World!' });
     return next();
 })
+
+server.get('/brick/:id', (req, res, next) => {
+    db.collection('bricks').doc(req.params.id).get()
+        .then((b) => {
+            brick = b.data();
+            Promise.all(brick.questions.map(async (question, i) => {
+                await question.component.get().then((component) => {
+                    brick.questions[i].component = component.data();
+                })
+            })).then(() => {
+                res.send(brick);
+            });
+        });
+});
 
 server.listen(8080, function() {
     console.log("%s listening at %s", server.name, server.url);
